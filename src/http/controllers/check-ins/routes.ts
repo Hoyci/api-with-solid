b6bac9241c30fa9afe98a1a3bd23e24ec84de692
Ruan Pablo <ruan.pablo.pec@hotmail.com>
2023-10-30 19:21:23 -0300

    finished project and added CI for unit tests

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -4,6 +4,7 @@ import { create } from './create'
 import { validate } from './validate'
 import { metrics } from './metrics'
 import { history } from './history'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verififyJWT)
@@ -11,7 +12,11 @@ export async function checkInsRoutes(app: FastifyInstance) {
   app.get('/check-ins/history', history)
   app.get('/check-ins/metrics', metrics)
 
-  app.patch('/check-ins/:checkInId/validate', validate)
+  app.patch(
+    '/check-ins/:checkInId/validate',
+    { onRequest: [verifyUserRole('ADMIN')] },
+    validate,
+  )
 
   app.post('/gyms/:gymId/check-ins', create)
 }
